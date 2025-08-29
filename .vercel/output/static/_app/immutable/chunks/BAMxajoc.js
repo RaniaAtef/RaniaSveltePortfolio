e6347ec5f4f6fff099f2c37e668af55b85983531
Rania atef <raniaatef718@gmail.com>
2025-08-30 00:07:25 +0300

    Fix Vercel deployment: switch to adapter-vercel and specify Node 20 runtime

---
+++ .vercel/output/static/_app/immutable/chunks/BAMxajoc.js
@@ -0,0 +1,1 @@
+import{l as a}from"./JfNkJZVo.js";a();
--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,11 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter({
-			runtime: 'nodejs20.x'   // 👈 Force Node 20
-		  }),
+			runtime: 'nodejs20.x'
+		}),
 		prerender: {
 			handleHttpError: ({ path, referrer, message }) => {
 				// Ignore 404s for missing files during build
